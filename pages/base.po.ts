334fedb4818bda35702118fbe07d60827a9bee52
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2020-07-15 10:58:08 +0300

    upgrade base.po

--- pages/base.po.ts
+++ pages/base.po.ts
@@ -1,6 +1,10 @@
-import { browser, ElementFinder, protractor } from 'protractor';
+import { browser, ElementArrayFinder, ElementFinder, protractor } from 'protractor';
 
 export class BasePo {
+
+    private defaultTimeout: number = 4000;
+    private defaultUIRetries: number = 4;
+    private defaultELRetries: number = 10;
 
     public async navigateTo(url: string, timeout: number = 3000): Promise<void> {
         return browser.get(url, timeout);
@@ -18,9 +22,9 @@ export class BasePo {
         await browser.sleep(ms);
     }
 
-    public async waitForElementText(element: ElementFinder, text: string, { timeout = 4000, maxRetries = 4 } = {}) {
-        let currentRetry = 0;
-        let error;
+    public async waitForElementText(element: ElementFinder, text: string, { timeout = this.defaultTimeout, maxRetries = this.defaultUIRetries } = {}) {
+        let currentRetry: number = 0;
+        let error: Error;
         do {
             try {
                 await browser.wait(protractor.ExpectedConditions.textToBePresentInElement(element, text), timeout);
@@ -30,13 +34,12 @@ export class BasePo {
                 error = err;
             }
         } while (currentRetry < maxRetries);
-        throw new Error(`Element ${element.locator()} is not containing specified text: ${text}
-        after maximum retries (${maxRetries}), Error message: ${error.name.toString()}`);
+        throw new Error(`Element ${element.locator()} is not containing specified text: ${text} after maximum retries (${maxRetries}), Error message: ${error.name.toString()}`);
     }
 
-    public async waitForElementVisible(element: ElementFinder, { timeout = 4000, maxRetries = 4 } = {}) {
-        let currentRetry = 0;
-        let error;
+    public async waitForElementVisible(element: ElementFinder, { timeout = this.defaultTimeout, maxRetries = this.defaultUIRetries } = {}) {
+        let currentRetry: number = 0;
+        let error: Error;
         do {
             try {
                 await browser.wait(protractor.ExpectedConditions.and(
@@ -48,13 +51,12 @@ export class BasePo {
                 error = err;
             }
         } while (currentRetry < maxRetries);
-        throw new Error(`Element ${element.locator()} is not visible
-        after maximum retries (${maxRetries}), Error message: ${error.name.toString()}`);
+        throw new Error(`Element ${element.locator()} is not visible after maximum retries (${maxRetries}), Error message: ${error.name.toString()}`);
     }
 
-    public async waitForElementInvisible(element: ElementFinder, { timeout = 4000, maxRetries = 4 } = {}) {
-        let currentRetry = 0;
-        let error;
+    public async waitForElementInvisible(element: ElementFinder, { timeout = this.defaultTimeout, maxRetries = this.defaultUIRetries } = {}) {
+        let currentRetry: number = 0;
+        let error: Error;
         do {
             try {
                 await browser.wait(protractor.ExpectedConditions.invisibilityOf(element), timeout);
@@ -64,13 +66,12 @@ export class BasePo {
                 error = err;
             }
         } while (currentRetry < maxRetries);
-        throw new Error(`Element ${element.locator()} is still visible
-        after maximum retries (${maxRetries}), Error message: ${error.name.toString()}`);
+        throw new Error(`Element ${element.locator()} is still visible after maximum retries (${maxRetries}), Error message: ${error.name.toString()}`);
     }
 
-    public async waitForElementClickable(element: ElementFinder, { timeout = 4000, maxRetries = 4 } = {}) {
-        let currentRetry = 0;
-        let error;
+    public async waitForElementClickable(element: ElementFinder, { timeout = this.defaultTimeout, maxRetries = this.defaultUIRetries } = {}) {
+        let currentRetry: number = 0;
+        let error: Error;
         do {
             try {
                 await browser.wait(protractor.ExpectedConditions.elementToBeClickable(element), timeout);
@@ -80,15 +81,48 @@ export class BasePo {
                 error = err;
             }
         } while (currentRetry < maxRetries);
-        throw new Error(`Element ${element.locator()} is not clickable
-        after maximum retries (${maxRetries}), Error message: ${error.name.toString()}`);
+        throw new Error(`Element ${element.locator()} is not clickable after maximum retries (${maxRetries}), Error message: ${error.name.toString()}`);
+    }
+
+    public async waitForListCountChanged(list: ElementArrayFinder, expectedCount: number, { timeout = this.defaultTimeout, maxRetries = this.defaultUIRetries } = {}) {
+        let currentRetry: number = 0;
+        let error: Error;
+        do {
+            try {
+                await browser.wait(() => {
+                    return list.count().then((actualValue) => {
+                        return actualValue === expectedCount;
+                    });
+                }, timeout);
+                return;
+            } catch (err) {
+                currentRetry++;
+                error = err;
+            }
+        } while (currentRetry < maxRetries);
+        throw new Error(`List ${list.locator()} count does not changed after maximum retries (${maxRetries}), Error message: ${error.name.toString()}`);
+    }
+
+    public async waitForElementToBeSelected(tgtElement: ElementFinder, { timeout = this.defaultTimeout, maxRetries = this.defaultUIRetries } = {}) {
+        let currentRetry: number = 0;
+        let error: Error;
+        do {
+            try {
+                await browser.wait(protractor.ExpectedConditions.elementToBeSelected(tgtElement), timeout);
+                return;
+            } catch (err) {
+                currentRetry++;
+                error = err;
+            }
+        } while (currentRetry < maxRetries);
+        throw new Error(`Element ${tgtElement.locator()} wasn't selected after maximum retries (${maxRetries}), Error message: ${error.name.toString()}`);
     }
 
     constructor() {
         console.log('BasePo');
     }
 
-    public async isElementPresent(element: ElementFinder, maxRetries = 10): Promise<boolean> {
+    public async isElementPresent(element: ElementFinder, maxRetries = this.defaultELRetries): Promise<boolean> {
         let res;
         for (let i = 0; i < maxRetries; i++) {
             res = await element.isPresent();
@@ -99,7 +133,7 @@ export class BasePo {
         return false;
     }
 
-    public async isElementDisplayed(element: ElementFinder, maxRetries = 10): Promise<boolean> {
+    public async isElementDisplayed(element: ElementFinder, maxRetries = this.defaultELRetries): Promise<boolean> {
         await this.isElementPresent(element);
         let res;
         for (let i = 0; i < maxRetries; i++) {
@@ -111,44 +145,44 @@ export class BasePo {
         return false;
     }
 
-    public async isElementEnabled(element: ElementFinder, timeout?: number): Promise<boolean> {
-        await this.isElementPresent(element, timeout);
+    public async isElementEnabled(element: ElementFinder): Promise<boolean> {
+        await this.isElementPresent(element);
         return element.isEnabled();
     }
 
-    public async isElementChecked(element: ElementFinder, timeout?: number): Promise<boolean> {
-        await this.isElementPresent(element, timeout);
+    public async isElementSelected(element: ElementFinder): Promise<boolean> {
+        await this.isElementPresent(element);
         return element.isSelected();
     }
 
-    public async getElementText(element: ElementFinder, timeout?: number): Promise<string> {
-        await this.isElementPresent(element, timeout);
+    public async getElementText(element: ElementFinder): Promise<string> {
+        await this.isElementPresent(element);
         return element.getText();
     }
 
-    public async clearElementField(element: ElementFinder, timeout?: number): Promise<void> {
-        await this.isElementPresent(element, timeout);
+    public async clearElementField(element: ElementFinder): Promise<void> {
+        await this.isElementPresent(element);
         await element.clear();
     }
 
-    public async typeElementText(element: ElementFinder, text: string | number, timeout?: number): Promise<void> {
-        await this.isElementPresent(element, timeout);
+    public async typeElementText(element: ElementFinder, text: string | number): Promise<void> {
+        await this.isElementPresent(element);
         await this.clearElementField(element);
         await element.sendKeys(text);
     }
 
-    public async getElementCssValue(element: ElementFinder, value: string, timeout?: number): Promise<string> {
-        await this.isElementPresent(element, timeout);
+    public async getElementCssValue(element: ElementFinder, value: string): Promise<string> {
+        await this.isElementPresent(element);
         return element.getCssValue(value);
     }
 
-    public async getElementValue(element: ElementFinder, timeout?: number): Promise<string> {
-        await this.isElementPresent(element, timeout);
+    public async getElementValue(element: ElementFinder): Promise<string> {
+        await this.isElementPresent(element);
         return element.getAttribute('value');
     }
 
-    public async getElementAttribute(element: ElementFinder, name: string, timeout?: number): Promise<string> {
-        await this.isElementPresent(element, timeout);
+    public async getElementAttribute(element: ElementFinder, name: string): Promise<string> {
+        await this.isElementPresent(element);
         return element.getAttribute(name);
     }
 
