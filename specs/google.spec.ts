be72dd8414ae10fa2d200f953a6a27fe27d5b7f2
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2020-11-19 14:53:58 +0200

    step 3

--- specs/google.spec.ts
+++ specs/google.spec.ts
@@ -10,11 +10,29 @@ describe('Google', () => {
         await browser.get('https://google.com', 2000);
         expect(await browser.getCurrentUrl()).toContain("https://www.google.com/");
         expect(await browser.getTitle()).toEqual("Google");
+    });
+
+    it('verify is element displayed', async () => {
         expect(await element(by.css('#hplogo')).isDisplayed()).toEqual(true);
+    });
+
+    it('verify element state', async () => {
         expect(await element.all(by.css('.gNO89b')).get(1).isEnabled()).toEqual(true);
+    });
+
+    it('verify input value', async () => {
         expect(await element(by.css('.gLFyf.gsfi')).getAttribute('value')).toEqual('');
+    });
+
+    it('verify image src', async () => {
         expect(await element(by.css('#hplogo')).getAttribute('src')).toContain('/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png');
+    });
+
+    it('verify element text', async () => {
         expect(await element(by.css('.Q8LRLc')).getText()).toEqual("Україна");
+    });
+
+    it('verify css properties', async () => {
         expect(await element(by.css('.Q8LRLc')).getCssValue('font-size')).toEqual("15px");
     });
 
