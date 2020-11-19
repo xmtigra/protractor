e16573904e0a6a001861fde803e90867c1399985
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2020-11-19 14:51:48 +0200

    step 1

--- specs/google.spec.ts
+++ specs/google.spec.ts
@@ -1,3 +1,5 @@
+import {browser, by, element} from "protractor";
+
 describe('Google', () => {
 
     beforeAll(async () => {
@@ -5,15 +7,16 @@ describe('Google', () => {
     });
 
     it('Go to google.com page ', async () => {
-        // code
+        await browser.get('https://google.com', 2000);
     });
 
     it('Fill some text in the input search', async () => {
-        // code
+        await element(by.css('.gLFyf.gsfi')).sendKeys('techmagic');
     });
 
     it('Click on the submit button', async () => {
-        // code
+        await element.all(by.css('.gNO89b')).get(1).click();
+        await browser.sleep(3000);
     });
 
     afterAll(async () => {
