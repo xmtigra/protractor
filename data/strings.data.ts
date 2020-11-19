b576ea21298a2bb9a4c9b0dbebafbe24d8bcac8b
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2020-11-19 14:56:05 +0200

    step 4

--- data/strings.data.ts
+++ data/strings.data.ts
@@ -10,3 +10,15 @@ export enum UI {
     WHITE_COLOR = 'rgba(255,255,255,1)',
     BUTTON_HEIGHT = '40px',
 }
+
+export enum GOOGLE {
+    URL = "https://www.google.com/",
+    SEARCH_TEXT = "techtagic",
+    TITLE = "Google",
+    COUNTRY = "Україна",
+    LOGO_SRC = "/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+}
+
+export enum GOOGLE_UI {
+    FONT_SIZE = "15px",
+}
--- specs/google.spec.ts
+++ specs/google.spec.ts
@@ -1,15 +1,19 @@
 import {browser, by, element} from "protractor";
+import {GOOGLE, GOOGLE_UI} from "../data/strings.data";
 
 describe('Google', () => {
+
+    const testSearchWorld = "techmagic";
+    const testSearchSite = "www.techmagic.co";
 
     beforeAll(async () => {
         console.log('if you need, add some preconditions before the test');
     });
 
     it('Go to google.com page ', async () => {
-        await browser.get('https://google.com', 2000);
-        expect(await browser.getCurrentUrl()).toContain("https://www.google.com/");
-        expect(await browser.getTitle()).toEqual("Google");
+        await browser.get(GOOGLE.URL, 2000);
+        expect(await browser.getCurrentUrl()).toContain(GOOGLE.URL);
+        expect(await browser.getTitle()).toEqual(GOOGLE.TITLE);
     });
 
     it('verify is element displayed', async () => {
@@ -25,26 +29,26 @@ describe('Google', () => {
     });
 
     it('verify image src', async () => {
-        expect(await element(by.css('#hplogo')).getAttribute('src')).toContain('/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png');
+        expect(await element(by.css('#hplogo')).getAttribute('src')).toContain(GOOGLE.LOGO_SRC);
     });
 
     it('verify element text', async () => {
-        expect(await element(by.css('.Q8LRLc')).getText()).toEqual("Україна");
+        expect(await element(by.css('.Q8LRLc')).getText()).toEqual(GOOGLE.COUNTRY);
     });
 
     it('verify css properties', async () => {
-        expect(await element(by.css('.Q8LRLc')).getCssValue('font-size')).toEqual("15px");
+        expect(await element(by.css('.Q8LRLc')).getCssValue('font-size')).toEqual(GOOGLE_UI.FONT_SIZE);
     });
 
     it('Fill some text in the input search', async () => {
-        await element(by.css('.gLFyf.gsfi')).sendKeys('techmagic');
-        expect(await element(by.css('.gLFyf.gsfi')).getAttribute('value')).toEqual('techmagic');
+        await element(by.css('.gLFyf.gsfi')).sendKeys(testSearchWorld);
+        expect(await element(by.css('.gLFyf.gsfi')).getAttribute('value')).toEqual(testSearchWorld);
     });
 
     it('Click on the submit button', async () => {
         await element.all(by.css('.gNO89b')).get(1).click();
-        await browser.sleep(3000);
-        expect(await element(by.css('[data-hveid="CAIQAA"] .iUh30.Zu0yb.tjvcx')).getText()).toContain('www.techmagic.co');
+        await browser.sleep(2000);
+        expect(await element(by.css('[data-hveid="CAIQAA"] .iUh30.Zu0yb.tjvcx')).getText()).toContain(testSearchSite);
     });
 
     afterAll(async () => {
